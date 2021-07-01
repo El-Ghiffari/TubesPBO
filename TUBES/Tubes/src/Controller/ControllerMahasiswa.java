aef32472ad15fa65a4e07114c75722ce8bf110b3
Rara El-Ghiffari <49091818+El-Ghiffari@users.noreply.github.com>
2021-07-01 13:28:10 +0700

    Update ControllerMahasiswa.java

--- TUBES/Tubes/src/Controller/ControllerMahasiswa.java
+++ TUBES/Tubes/src/Controller/ControllerMahasiswa.java
@@ -24,7 +24,7 @@ import javax.swing.JOptionPane;
 
 /**
  *
- * @author satria
+ * @author satria, deni
  */
 public class ControllerMahasiswa extends MouseAdapter implements ActionListener{
     private GUIMahasiswa view;
